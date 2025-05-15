5cd75dff7f086a533274e93f9bd8b6efd2d125a5
Mohamed Radwan <148477116+DevRADWAN20@users.noreply.github.com>
2025-05-15 07:22:17 +0300

    Add files via upload
    task one completed

---
+++ task_1.php
@@ -0,0 +1,19 @@
+<?php
+
+
+// Define variables and give them values
+$a = 10;
+$b = 3;
+
+// Applying and print all mathematical operations
+echo "a = $a<br>";
+echo "b = $b<br><br>";
+
+
+echo " (a + b): " . ($a + $b) . "<br>";     // Addition Output = 13
+echo " (a - b): " . ($a - $b) . "<br>";      // Subtraction Output = 7
+echo " (a * b): " . ($a * $b) . "<br>";  // Multiplication Output = 30
+echo " (a / b): " . ($a / $b) . "<br>";  // Division Output = 3.33
+echo " (a % b): " . ($a % $b) . "<br>"; // Modulus Output = 1
+echo " (a ** b): " . ($a ** $b) . "<br>"; // Exponentiation Output = 1000
+
